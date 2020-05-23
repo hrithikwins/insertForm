babfad38aa8a731df27684c94b56dca61e5d8413
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2020-05-23 16:57:01 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'App.css';
+import AddEmployee from 'views/AddEmployee/AddEmployee';
+
+function App() {
+  return (
+    <>
+    {'Welcome to this form'}
+    <AddEmployee />
+    </>
+  );
+}
+
+export default App;
